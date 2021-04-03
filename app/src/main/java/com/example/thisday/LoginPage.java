0f0f82d8cc9ff127b282ffbb4383a75575478305
ullanes <ullanes@horizon.csueastbay.edu>
2021-04-02 17:24:22 -0700

    set on click listener for login and register button. hide actionbar.

--- app/src/main/java/com/example/thisday/LoginPage.java
+++ app/src/main/java/com/example/thisday/LoginPage.java
@@ -1,6 +1,10 @@
 package com.example.thisday;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +12,53 @@ import androidx.appcompat.app.AppCompatActivity;
 public class LoginPage extends AppCompatActivity {
 
     private TextView mTextView;
+    private EditText etPassword;
+    private EditText etUsername;
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
 
         mTextView = (TextView) findViewById(R.id.text);
+        etPassword = findViewById(R.id.etPassword);
+        etUsername =findViewById(R.id.etUsername);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
+
+
+    }
+
+
+    private void loginUser(){
+
+        Intent i = new Intent(this, FeedsActivity.class);
+        startActivity(i);
+    }
+
+    private void registerUser(){
+        Intent i = new Intent(this, RegisterPage.class);
+        startActivity(i);
+
     }
 }
