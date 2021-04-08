14175262617907009e1f63b0d7ae75e2a40902b3
ullanes <ullanes@horizon.csueastbay.edu>
2021-04-07 17:07:47 -0700

    commiting

--- app/src/main/java/com/example/thisday/LoginPage.java
+++ app/src/main/java/com/example/thisday/LoginPage.java
@@ -2,12 +2,18 @@ package com.example.thisday;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -25,7 +31,7 @@ public class LoginPage extends AppCompatActivity {
 
         mTextView = (TextView) findViewById(R.id.text);
         etPassword = findViewById(R.id.etPassword);
-        etUsername =findViewById(R.id.etUsernameR);
+        etUsername =findViewById(R.id.etUsername);
         btnLogin = findViewById(R.id.btnLogin);
         btnRegister = findViewById(R.id.btnRegister);
 
@@ -33,7 +39,9 @@ public class LoginPage extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginUser();
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username,password);
 
             }
         });
@@ -50,12 +58,30 @@ public class LoginPage extends AppCompatActivity {
     }
 
 
-    private void loginUser(){
+    private void loginUser(String username, String password){
+        Log.i("Login", "Attempting to login" +  username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
 
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+                if(e != null){
+                    Log.e("Login", "Issue with Login", e);
+                    return;
+                }
+                gotoMainActivity();
+
+
+                Toast.makeText(LoginPage.this,"successfully login", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
+    private void gotoMainActivity(){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+
+    }
     private void registerUser(){
         Intent i = new Intent(this, RegisterPage.class);
         startActivity(i);
--- app/src/main/java/com/example/thisday/MainActivity.java
+++ app/src/main/java/com/example/thisday/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottomNavigation);
+        getSupportActionBar().hide();
 
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
